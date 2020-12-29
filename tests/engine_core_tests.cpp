7487448b1e5ffb491e4e9310618235cdfa5e2686
Alexis Crozet <alexis.crozet@epitech.eu>
2020-12-29 17:57:08 +0100

    CORE/tools +(boost test lib auto installed with conan) +(basic sample test)

---
+++ tests/engine_core_tests.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE Engine Core Tests
+#include <boost/test/included/unit_test.hpp>
+#include "Game.hpp"
+
+// A test to test the test framework ? damn this is meta
+BOOST_AUTO_TEST_CASE(test_test)
+{
+    BOOST_TEST(true);
+}
