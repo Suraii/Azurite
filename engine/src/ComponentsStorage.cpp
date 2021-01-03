e0bac5a89060a9703a1ff8238d050e90b94c4c5d
Alexis Crozet <alexis.crozet@epitech.eu>
2021-01-03 13:15:29 +0100

    CORE/components_storage +(ComponentsStorage class blank implementation)

---
+++ engine/src/ComponentsStorage.cpp
@@ -0,0 +1,12 @@
+#include "ComponentsStorage.hpp"
+
+using namespace Azurite;
+
+ComponentsStorage::ComponentsStorage(Game &owner) : m_owner(owner)
+{}
+
+ComponentsStorage::~ComponentsStorage() {}
+
+//EntityBuilder ComponentsStorage::buildEntity() {}
+
+void ComponentsStorage::destroyEntity(unsigned id) {}
