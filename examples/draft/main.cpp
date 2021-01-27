d3683938bece748bc6d530f88eeb26c1ff49d166
Alexis Crozet <alexis.crozet@epitech.eu>
2021-01-27 10:20:30 +0100

    DRAFT/modules +(Sfmlmodule in game)

--- examples/draft/main.cpp
+++ examples/draft/main.cpp
@@ -1,6 +1,13 @@
 #include "Azurite/Game.hpp"
+#include "Azurite/modules/SfmlModule.hpp"
+#include <memory>
 
 int main()
 {
     Azurite::Game game;
+    std::unique_ptr<Azurite::AModule> sfml_mod(new Azurite::SfmlModule(game));
+
+    game.addModule("sfml", std::move(sfml_mod));
+
+    game.run();
 }
