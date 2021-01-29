a43c8c3b0e9ea93eaeefcb8caf7929501fbcf150
Alexis Crozet <alexis.crozet@epitech.eu>
2021-01-29 14:17:15 +0100

    BUILT-IN/components +(CSprite & CTransform2D)

---
+++ engine/include/Azurite/components/CSprite.hpp
@@ -0,0 +1,15 @@
+#ifndef __AZURITE__BUILT_IN_COMPONENTS_SPRITE
+#define __AZURITE__BUILT_IN_COMPONENTS_SPRITE
+
+#include <string>
+
+namespace Azurite {
+
+struct CSprite {
+    std::string spritesheet;
+    unsigned id = 0;
+};
+
+}
+
+#endif
