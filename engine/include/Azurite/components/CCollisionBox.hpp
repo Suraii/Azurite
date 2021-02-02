296617233ccb7b57460bda583eb3f5a1e3ea4a5f
Alexis Crozet <alexis.crozet@epitech.eu>
2021-02-02 10:15:41 +0100

    BUILT-IN/components +(Collision Box component)

---
+++ engine/include/Azurite/components/CCollisionBox.hpp
@@ -0,0 +1,14 @@
+#ifndef __AZURITE__BUILT_IN_COLLISION_BOX
+#define __AZURITE__BUILT_IN_COLLISION_BOX
+
+#include "Azurite/Types.hpp"
+
+namespace Azurite {
+
+struct CCollisionBox {
+    Vector2D size = {1, 1};
+};
+
+}
+
+#endif
