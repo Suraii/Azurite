f1551069b2e34963fa7d8f5deb1aead1fbe94f38
Alexis Crozet <alexis.crozet@epitech.eu>
2021-02-16 12:33:13 +0100

    CLI/commands +(implemented help)

---
+++ cli/.azurite.d/help.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# AZURITE CLI HELP SCRIPT
+
+source $AZURITE_DIR/../azurite "__including"
+
+azurite_raw "§lhelp\t: §ndisplay this menu\n"
+azurite_raw "§linstall\t: §ninstall the latest version of the Azurite engine library\n"
+azurite_raw "§lupdate\t: §nupdate the current installation if needed\n"
+azurite_raw "§lremove\t: §ndestroy all Azurite engine library's files\n"
+azurite_raw "§lversion\t: §nprint the version of your current Azurite installation\n"
+
+
